75ec0e4f5a06b5c0090951cd2d54bf9c3770672a
LeonardoMigueis <leomigueis@hotmail.com>
2017-06-05 12:17:20 -0300

    Add files via upload

--- alien.lua
+++ alien.lua
@@ -1,19 +1,37 @@
-local alien_move = {}
-local alien_anim_frame = 1
-local alien_pos_x = 812
-local alien_pos_y = 230
-local alien_anim_time = 0
-local alien_dir= -1
- function alien_move.load()
-  love.graphics.setBackgroundColor(126,189,252)
+ local alien_move = {}
+ local alien_anim_frame = 1
+ local alien_pos_x = 630
+ local alien_pos_y = 370
+ local alien_anim_time = 0
+ local alien_dir= -1
+ local function hipotenusa(x1, x2, y1, y2)
+   local dist = math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+   return dist
+   end 
+function alien_move.load()
  for x=1,4,1 do
    alien_move[x] = love.graphics.newImage("alien_move_0" .. x .. ".png")
    end
  end
-function alien_move.update(dt)
-   if alien_pos_x - 280 < 0 then
-      alien_pos_x = 280
+function alien_move.update(dt, girl_pos_x, girl_pos_y)
+   if alien_pos_x - 125 < 0 then
+      alien_pos_x = 125
      end 
+     
+  d = hipotenusa(alien_pos_x, girl_pos_x, alien_pos_y, girl_pos_y)
+ if d < 500 then
+  alien_dir = -1
+  alien_pos_x = alien_pos_x - (130*dt)
+  alien_anim_time = alien_anim_time + dt
+    if alien_anim_time > 0.2 then
+      alien_anim_frame = alien_anim_frame + 1
+      if alien_anim_frame > 4 then
+          alien_anim_frame = 1
+       end
+       alien_anim_time = 0
+     end
+   end
+ --[[  
    if love.keyboard.isDown("right") then
      alien_dir = 1
      alien_pos_x = alien_pos_x + (100*dt)
@@ -37,14 +55,14 @@ function alien_move.update(dt)
            end
          alien_anim_time = 0
        end
-     end 
+     end --]]
 end
- function alien_move.keyreleased(key)
+function alien_move.keyreleased(key)
    if key == "left" or key == "right" then
      alien_anim_frame = 1
    end
   end
 function alien_move.draw()
-  love.graphics.draw(alien_move[alien_anim_frame], alien_pos_x, alien_pos_y, 0, 2.5*alien_dir, 2.5)
+  love.graphics.draw(alien_move[alien_anim_frame], alien_pos_x, alien_pos_y, 0, 2.5*alien_dir, 2.5,alien_move[alien_anim_frame]:getWidth()/2, alien_move[alien_anim_frame]:getHeight ()/2)
 end
-return alien_move
+ return alien_move
--- girl.lua
+++ girl.lua
@@ -1,9 +1,12 @@
-local girl_walk = {}
-local girl_anim_frame = 1
-local girl_pos_x = 70
-local girl_pos_y = 357
-local girl_anim_time = 1
-local girl_dir = 1
+local girl_walk       = {}
+girl_anim_frame = 1
+girl_pos_x      = 70
+girl_pos_y      = 100
+girl_anim_time  = 1
+girl_dir        = 1
+local gravity         = 200
+local girl_vel_y      = 200
+local collided        = false
 
 function girl_walk.load()
   for x = 1,9,1 do
@@ -11,12 +14,20 @@ function girl_walk.load()
   end
 end
 function girl_walk.update(dt)
-  if girl_pos_x - 65/2 < 0 then
-    girl_pos_x = 65/2
-  end
+  if girl_pos_x - 50 < 0 then
+     girl_pos_x = 50
+   end 
+  if girl_pos_y > 487 - 0.253*girl_walk[1]:getHeight()/2 + 5 then
+     girl_pos_y = 487 - 0.253 *girl_walk[1]:getHeight()/2 + 5
+     collided = true
+   end
+  if not collided then
+    girl_vel_y = girl_vel_y + gravity*dt
+    girl_pos_y = girl_pos_y + girl_vel_y*dt
+    end
   if love.keyboard.isDown("d") then
     girl_dir = 1
-    girl_pos_x = girl_pos_x + (100 * dt)
+    girl_pos_x = girl_pos_x + (170 * dt)
     girl_anim_time = girl_anim_time + dt
     if girl_anim_time > 0.2 then
       girl_anim_frame = girl_anim_frame + 1
@@ -28,7 +39,7 @@ function girl_walk.update(dt)
   end
   if love.keyboard.isDown("a") then
     girl_dir = -1
-    girl_pos_x = girl_pos_x + (100 * girl_dir* dt)
+    girl_pos_x = girl_pos_x + (170 * girl_dir* dt)
     girl_anim_time = girl_anim_time + dt
     if girl_anim_time > 0.2 then
       girl_anim_frame = girl_anim_frame + 1
@@ -36,15 +47,21 @@ function girl_walk.update(dt)
         girl_anim_frame = 2
       end
       girl_anim_time = 0.14
-    end
-  end
+     end
 end
- function girl_walk.keyreleased(key)
+end
+function girl_walk.keypressed(key) 
+  if (key == "w") and collided then
+    collided   = false
+    girl_vel_y = -200
+ end
+end 
+function girl_walk.keyreleased(key)
    if key == "a" or key == "d" then
-     girl_anim_frame = 1
+      girl_anim_frame = 1
    end
  end
-function girl_walk.draw ()
-    love.graphics.draw(girl_walk[girl_anim_frame], girl_pos_x, girl_pos_y,0,0.253 * girl_dir,0.253)
+function girl_walk.draw()
+    love.graphics.draw(girl_walk[girl_anim_frame], girl_pos_x, girl_pos_y,0,0.253 * girl_dir,0.253,girl_walk[girl_anim_frame]:getWidth()/2, girl_walk[girl_anim_frame]:getHeight ()/2)
 end
-return girl_walk
+  return girl_walk
