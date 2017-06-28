4c434b2c02b1193491bf09d24be188f475284347
Jvictor22 <jvictor_dias55@hotmail.com>
2017-06-28 12:35:17 -0300

    Add files via upload

--- alien.lua
+++ alien.lua
@@ -1,71 +1,91 @@
-local alien_move = {}
- local alien_anim_frame = 1
- local alien_pos_x = 630
- local alien_pos_y = 370
- local alien_anim_time = 0
- local alien_dir= -1
+alien = {}
+alien.anim_frame = 1
+alien.posX = 630
+alien.posY = 370
+alien.anim_time = 0
+alien.dir= -1
+
+local enemy_bullets = {}
+
+local cauda = {}
+cauda.anim_frame =  1
+cauda.x      = alien.posX
+cauda.y      = alien.posY
+cauda.anim_time = 1
+cauda.dir =  1
+
+local tiroAlien = {} 
+atirar = true
+delayTiro = 0.1
+timeAtirar = delayTiro
+imgTiroAlien = love.graphics.newImage( "tiro_alien.png" )
+
+function bulletSpawn(x,y)
+  table.insert(enemy_bullets, {x=x, y=y})
+end
+
 local function hipotenusa(x1, x2, y1, y2)
    local dist = math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
    return dist
    end 
-function alien_move.load()
+function alien.load()
  for x=1,4,1 do
-   alien_move[x] = love.graphics.newImage("alien_move_0" .. x .. ".png")
-   end
+   alien[x] = love.graphics.newImage("alien_move_0" .. x .. ".png")
  end
-function alien_move.update(dt, girl_pos_x, girl_pos_y)
-   if alien_pos_x - 125 < 0 then
-      alien_pos_x = 125
+ for i=1,6,1 do
+   cauda[i] = love.graphics.newImage("cauda_0" .. i .. ".png")
+ end
+ end 
+function alien.update(dt, girl_pos_x, girl_pos_y)
+   if alien.posX - 125 < 0 then
+      alien.posX = 125
      end
-   d = hipotenusa(alien_pos_x, girl_pos_x, alien_pos_y, girl_pos_y)
-   dir = girl_pos_x - alien_pos_x
+   d = hipotenusa(alien.posX, girl_pos_x, alien.posY, girl_pos_y)
+   dir = girl_pos_x - alien.posX
    if dir ~= 0 then
-      alien_dir = dir / math.abs(dir)
-   if d < 400 then
-     alien_pos_x = alien_pos_x + (100*dt*alien_dir)
-     alien_anim_time = alien_anim_time + dt
-     if alien_anim_time > 0.2 then
-       alien_anim_frame = alien_anim_frame + 1
-       if alien_anim_frame > 4 then
-          alien_anim_frame = 1
+alien.dir = dir / math.abs(dir)
+ if d < 400 then
+     alien.posX = alien.posX + (100*dt*alien.dir)
+     alien.anim_time = alien.anim_time + dt
+     if alien.anim_time > 0.2 then
+       alien.anim_frame = alien.anim_frame + 1
+       if alien.anim_frame > 4 then
+          alien.anim_frame = 1
        end
-       alien_anim_time = 0
-     end
-   end
+       alien.anim_time = 0
+     end 
    end 
- --[[
-   if love.keyboard.isDown("right") then
-     alien_dir = 1
-     alien_pos_x = alien_pos_x + (100*dt)
-     alien_anim_time = alien_anim_time + dt
-     if alien_anim_time > 0.2 then
-       alien_anim_frame = alien_anim_frame + 1
-       if alien_anim_frame > 4 then
-          alien_anim_frame = 1
-       end
-       alien_anim_time = 0
-     end
-   end
-   if love.keyboard.isDown("left") then
-     alien_dir = -1
-     alien_pos_x = (alien_pos_x) - (100*dt)
-     alien_anim_time = alien_anim_time + dt
-      if alien_anim_time > 0.2 then
-         alien_anim_frame = alien_anim_frame + 1
-         if alien_anim_frame > 4 then
-            alien_anim_frame = 1
-           end
-         alien_anim_time = 0
-       end
-     end
-     ]]--
+   d2 = d / 2
+  if d2 < 250 then
+    cauda.dir = alien.dir
+    cauda.x = alien.posX + (dt)
+    cauda.anim_time = cauda.anim_time + dt
+    if cauda.anim_time > 0.2 then
+      cauda.anim_frame = cauda.anim_frame + 1
+      if cauda.anim_frame > 6 then
+        cauda.anim_frame = 1
+      end
+      cauda.anim_time = 0.0001
+    end
+   end 
+ end
+ for i=1, #enemy_bullets do
+   --fazer a bullet andar
+   
+ end
 end
-function alien_move.keyreleased(key)
+function alien.keyreleased(key)
    if key == "left" or key == "right" then
-     alien_anim_frame = 1
+     alien.anim_frame = 1
    end
   end
-function alien_move.draw()
-  love.graphics.draw(alien_move[alien_anim_frame], alien_pos_x, alien_pos_y, 0, 2.5*alien_dir, 2.5,alien_move[alien_anim_frame]:getWidth()/2, alien_move[alien_anim_frame]:getHeight ()/2)
+function alien.draw()
+ if d < 400 then 
+    love.graphics.draw(alien[alien.anim_frame], alien.posX, alien.posY, 0, 2.5*alien.dir, 2.5,alien[alien.anim_frame]:getWidth()/2, alien[alien.anim_frame]:getHeight ()/2)
+ elseif d2 < 250 then 
+      love.graphics.draw(cauda[cauda.anim_frame], alien.posX, alien.posY, 0, 2.5*cauda.dir, 2.5,cauda[cauda.anim_frame]:getWidth()/2, cauda[cauda.anim_frame]:getHeight ()/2)
+    else 
+       love.graphics.draw(alien[alien.anim_frame], alien.posX, alien.posY, 0, 2.5*alien.dir, 2.5,alien[alien.anim_frame]:getWidth()/2, alien[alien.anim_frame]:getHeight ()/2)
+      end 
 end
- return alien_move
+ return alien
--- main.lua
+++ main.lua
@@ -1,32 +1,61 @@
-local menu = require "menu"
-local game = require "game"
-
+local girl_walk  = require "girl"
+local alien      = require "alien"
+local enemy      = require "enemy"
+ sucess = love.window.setMode(800, 600, {resizable = true})
+          love.window.setTitle("Terra Brasilis")
+          
 function love.load()
-  menu.load()
-  game.load()
-  state = menu
+  alien.load()
+  
+  girl_walk.load()
+  
+  enemy.load()
+  
+  imagem_Cloud = love.graphics.newImage ("mario_cloud.png")
+  imagem_width = imagem_Cloud: getWidth()
+  imagem_height = imagem_Cloud : getHeight()
+  
+  background = love.graphics.newImage("sao_paulo_background.png")
+  largura = background: getWidth()
+  altura  = background: getHeight()
+  
+  imagem_ground = love.graphics.newImage ("ground.png")
+  imagem_width = imagem_ground: getWidth()
+  imagem_height = imagem_ground : getHeight()
+end 
+function love.update(dt)
+alien.update(dt, girl_pos_x, girl_pos_y)
+girl_walk.update(dt)
+enemy.update(dt)
+end
+function love.keyreleased(key)
+ alien.keyreleased(key)
+ girl_walk.keyreleased(key)
 end
 
-function love.update(dt)
-  state.update(dt)
-end
+
+
 
 function love.draw()
-  state.draw()
-end
+ love.graphics.draw(background, 0, 00, 0,1.78,5)
 
-function love.changeToGame()
-	state = game
-end
-
-function love.mousepressed(x,y,but)
-	state.mousepressed(x,y,but)
-end
-
-function love.keypressed(key)
-  state.keypressed(key)
- end
+ love.graphics.draw (imagem_Cloud,140,-50)
+ love.graphics.draw (imagem_Cloud,490,-50)
  
-function love.keyreleased(key)
- state.keyreleased(key)
-end
+ alien.draw()
+ 
+ girl_walk.draw()
+ 
+ enemy.draw()
+ 
+ love.graphics.draw (imagem_ground,0,487)
+ love.graphics.draw (imagem_ground,220,487)
+ love.graphics.draw (imagem_ground,440,487)
+ love.graphics.draw (imagem_ground,660,487)
+end 
+function love.keypressed(key)
+  girl_walk.keypressed(key)
+  if (key=="escape") then
+    love.event.quit()
+    end
+ end
